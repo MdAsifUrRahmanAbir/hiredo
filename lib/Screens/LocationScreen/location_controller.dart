2942725fe742ead490a0ed4ee308b9e3d4aacf2a
limon <limon222020@gmail.com>
2023-04-17 18:07:24 +0600

    location

--- lib/Screens/LocationScreen/location_controller.dart
+++ lib/Screens/LocationScreen/location_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../JobPost/Model/location_model.dart';
 import '../UpdateLeadSetting/Model/location_model.dart';
 
 class LocationController extends GetxController {
@@ -10,5 +11,15 @@ class LocationController extends GetxController {
   TextEditingController cityController = TextEditingController();
   TextEditingController distanceController = TextEditingController();
 
-  List<LocationModel> country = [];
+  List<LocationDataModel> country = [
+    LocationDataModel(
+        location: "dhaka",
+        latitude: "23.813584728069344",
+        longitude: "90.41449459910878"),
+    LocationDataModel(
+        location: "India",
+        latitude: "22.85688857284059",
+        longitude: "79.23119074557933")
+  ];
+  LocationDataModel? locationData;
 }
--- lib/Services/api_component.dart
+++ lib/Services/api_component.dart
@@ -1,4 +1,3 @@
-
 String baseUrl = "http://ringknock.pythonanywhere.com/";
 String signupApi = "${baseUrl}register/";
 String signInApi = "${baseUrl}login/";
@@ -7,19 +6,14 @@ String serviceApi = "${baseUrl}services/";
 String fetchService = "${baseUrl}services/";
 String addLocation = "${baseUrl}locations/";
 String fetchLocation = "${baseUrl}locations/";
-String sliderApi="${baseUrl}slider/";
+String sliderApi = "${baseUrl}slider/";
 String fetchBedgeApi = "${baseUrl}profile/badge/";
-String jobPostApi="$baseUrl/lead/JobPostCreate/";
+String jobPostApi = "$baseUrl/lead/JobPostCreate/";
 
-
-
-String fetchAllLocationApi="${baseUrl}lead/PostLocation/";
+String fetchAllLocationApi = "${baseUrl}lead/PostLocation/";
 String textApi = "${baseUrl}lead/category/";
 String locationsApi = "${baseUrl}lead/category/";
-String forgotPasswordApi="${baseUrl}reset-password-otp/";
-
-
-
+String forgotPasswordApi = "${baseUrl}reset-password-otp/";
 
 String changePasswordApi = "${baseUrl}change-password/";
 String emailTemplateApi = "${baseUrl}email_templates/";
@@ -27,35 +21,21 @@ String smsTemplateApi = "${baseUrl}sms_templates/";
 String realTimeServiceApi = "${baseUrl}lead/RealTimeService/";
 String pendingPostApi = "${baseUrl}lead/PendingPost/";
 String profilePicPostApi = "${baseUrl}profile_pic_add/";
-
-
+String helpGetApi = "${baseUrl}profile/help/";
+String getLocationApi = "${baseUrl}locations/";
 
 const String stripeUrl = "https://api.stripe.com/v1/payment_intents";
 const String stripeSecretKey =
-        "sk_test_51MvmB4BjAkDiQKQNL5Jdp2HS8bbG4rDruFf43JGh03rX2PxoMHYul0kbUh7hRCi2T5ux8AFkfvmbWllvFqDaF2n000bKD76MjZ";
+    "sk_test_51MvmB4BjAkDiQKQNL5Jdp2HS8bbG4rDruFf43JGh03rX2PxoMHYul0kbUh7hRCi2T5ux8AFkfvmbWllvFqDaF2n000bKD76MjZ";
 
 const String stripePublishKey =
-        "pk_test_51MvmB4BjAkDiQKQNWHzjwWU9Pmmv3WtHEwXa3TzUqzAV83v3T0tzB2F8yPdV7CiDvdWvwwR4oaNzLhBex6wploqs0052FpuPxX";
+    "pk_test_51MvmB4BjAkDiQKQNWHzjwWU9Pmmv3WtHEwXa3TzUqzAV83v3T0tzB2F8yPdV7CiDvdWvwwR4oaNzLhBex6wploqs0052FpuPxX";
 
+String changeUserModeApi = "${baseUrl}profile/change-user-type/";
+String logoutUserApi = "${baseUrl}logout/";
+String leadsCountApi = "${baseUrl}ServiceWiseLeadCount/";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-String changeUserModeApi="${baseUrl}profile/change-user-type/";
-String logoutUserApi="${baseUrl}logout/";
-String leadsCountApi="${baseUrl}ServiceWiseLeadCount/";
-
-String needHelpsApi="${baseUrl}profile/still-need-help/";
-String profileApi="${baseUrl}profile/profile-update/";
-String deleteServiceApi="${baseUrl}services/";
-String leadsApi="${baseUrl}ServiceWiseLead/?page=";
+String needHelpsApi = "${baseUrl}profile/still-need-help/";
+String profileApi = "${baseUrl}profile/profile-update/";
+String deleteServiceApi = "${baseUrl}services/";
+String leadsApi = "${baseUrl}ServiceWiseLead/?page=";
