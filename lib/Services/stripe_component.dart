f01f8eb04bd2beee9484f5aef2231e7946ebcafc
palash-barman <palash900202@gmail.com>
2023-05-07 11:30:52 +0600

    payment card fetch completed

--- lib/Services/stripe_component.dart
+++ lib/Services/stripe_component.dart
@@ -2,3 +2,8 @@
 
 String createTokenApi="${stripeBaseUrl}v1/tokens";
 String createCardApi="${stripeBaseUrl}v1/customers/";
+
+ String fetchCardApi(String customerId){
+    return "${stripeBaseUrl}v1/customers/$customerId/sources?object=card";
+}
+
